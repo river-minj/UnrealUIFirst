e1a5d1349fcf937328e070445e4b7ea868944269
river-minj <hanr221@gmail.com>
2026-03-10 12:53:03 +0900

    탭버튼/ 탭화면 변경

--- First/Source/First/LobbyWidget.cpp
+++ First/Source/First/LobbyWidget.cpp
@@ -1,5 +1,54 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "LobbyWidget.h"
+#include "Components/Button.h"
+#include "Components/WidgetSwitcher.h"
 
+void ULobbyWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	UE_LOG(LogTemp, Log, TEXT("LobbyWidget NativeConstruct called!"));
+
+	if (ShopButton)
+	{
+		UE_LOG(LogTemp, Log, TEXT("ShopButton found!"));
+		ShopButton->OnClicked.AddDynamic(this, &ULobbyWidget::OnShopButtonClicked);
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("ShopButton is NULL!"));
+	}
+
+	if (InventoryButton)
+	{
+		InventoryButton->OnClicked.AddDynamic(this, &ULobbyWidget::OnInventoryButtonClicked);
+	}
+
+	if (SettingsButton)
+	{
+		SettingsButton->OnClicked.AddDynamic(this, &ULobbyWidget::OnSettingsButtonClicked);
+	}
+}
+
+void ULobbyWidget::OnShopButtonClicked()
+{
+	if (ContentSwitcher)
+	{
+		ContentSwitcher->SetActiveWidgetIndex(0);
+	}
+}
+
+void ULobbyWidget::OnInventoryButtonClicked()
+{
+	if (ContentSwitcher)
+	{
+		ContentSwitcher->SetActiveWidgetIndex(1);
+	}
+}
+
+void ULobbyWidget::OnSettingsButtonClicked()
+{
+	if (ContentSwitcher)
+	{
+		ContentSwitcher->SetActiveWidgetIndex(2);
+	}
+}
--- First/Source/First/LobbyWidget.h
+++ First/Source/First/LobbyWidget.h
@@ -1,17 +1,39 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
 #include "LobbyWidget.generated.h"
 
-/**
- * 
- */
+class UButton;
+class UWidgetSwitcher;
+
 UCLASS()
 class FIRST_API ULobbyWidget : public UUserWidget
 {
 	GENERATED_BODY()
-	
+
+public:
+	virtual void NativeConstruct() override;
+
+protected:
+	UPROPERTY(meta = (BindWidget))
+	UButton* ShopButton;
+
+	UPROPERTY(meta = (BindWidget))
+	UButton* InventoryButton;
+
+	UPROPERTY(meta = (BindWidget))
+	UButton* SettingsButton;
+
+	UPROPERTY(meta = (BindWidget))
+	UWidgetSwitcher* ContentSwitcher;
+
+	UFUNCTION()
+	void OnShopButtonClicked();
+
+	UFUNCTION()
+	void OnInventoryButtonClicked();
+
+	UFUNCTION()
+	void OnSettingsButtonClicked();
 };
